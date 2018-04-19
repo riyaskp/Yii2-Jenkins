a638a5ab83836a3fb955b6d77de6f928eebe9fbb
Tatweerintl\m.riyas <mriyas@fbd.net>
2018-04-19 10:23:35 +0300

    changes to read me

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=College',
+    'dsn' => 'mysql:host=localhost;dbname=yii_jenkins',
     'username' => 'root',
     'password' => '123',
     'charset' => 'utf8',
